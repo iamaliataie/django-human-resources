376f824f621e64b925ef19602576f891df80b256
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-21 06:02:48 +0430

    Update views.py

--- App/views.py
+++ App/views.py
@@ -7,7 +7,6 @@ from .forms import CandidateForm
 def home(request):
     
     form = CandidateForm(request.POST or None)
-    
     if request.method == 'POST':
         if form.is_valid():
             form.save()
