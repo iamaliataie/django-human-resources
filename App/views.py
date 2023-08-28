50b389c7cf1a24f45c0f88b3122ae8646d9eca33
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-28 14:48:58 +0430

    update home template and home view to send file with post request

--- App/views.py
+++ App/views.py
@@ -4,14 +4,17 @@ from django.contrib import messages
 from .forms import CandidateForm
 
 def home(request):
-    form = CandidateForm(request.POST or None)
     if request.method == 'POST':
+        form = CandidateForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "Your request was successful")
             return redirect('/')
         else:
             messages.error(request, "Something went wrong, please try again")
-    context = {'form': form}
-    
-    return render(request, "home.html", context)
+        context = {'form': form}
+        
+        return render(request, "home.html", context)
+    else:
+        form = CandidateForm()
+        return render(request, "home.html", {'form': form})
