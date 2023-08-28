17b551abbf6496d4538076b1434a9ba3e659bf37
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-28 14:44:28 +0430

    UPDATE settings and urls

--- Candidates/urls.py
+++ Candidates/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from App import views
@@ -5,4 +7,4 @@ from App import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
