9ec37e9584093c94db7069f280f21d58879b5e49
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-29 10:35:01 +0430

    add readonly fields to candidate admin form

--- App/admin.py
+++ App/admin.py
@@ -8,6 +8,8 @@ from .forms import CandidateForm
 
 class CandidateAdmin(admin.ModelAdmin):
     form = CandidateForm
+    readonly_fields = ('created_on', 'first_name', 'last_name', 'email', 'age', 'job', 'salary', 'gender', 'smoker', 'resume', 'messages', 'languages', 'frameworks', 'databases', 'libraries', 'mobile', 'other', 'personality', 'experience', 'phone',)
+    exclude = ('status',)
     list_filter = ('situation',)
     list_display = ('first_name', 'last_name', 'email', 'created_on', 'status', '_')
     search_fields = ('first_name', 'last_name', 'email', 'age',)
