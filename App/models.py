6cfd7b11c1d700c082199fe7808697551a748877
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-11-15 14:27:04 +0430

    Add finished_job field to candidate model

--- App/models.py
+++ App/models.py
@@ -118,6 +118,7 @@ class Candidate(models.Model):
     company = models.CharField(max_length=100)
     position = models.CharField(max_length=50)
     started_job = models.DateField(auto_now=False, auto_now_add=False)
+    finished_job = models.DateField(auto_now=False, auto_now_add=False)
 
     other = MultiSelectField(choices=OTHER, default='', max_length=20)
     situation = models.CharField(max_length=20, default='Pending', choices=SITUATION, null=True,)
