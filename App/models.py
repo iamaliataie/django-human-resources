237e933b520d5231817d04bfffae8d290d3761f2
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-11-15 14:24:32 +0430

    Add company field to candidate model

--- App/models.py
+++ App/models.py
@@ -114,6 +114,9 @@ class Candidate(models.Model):
     about_course = models.TextField()
     status_course = models.CharField(max_length=50, null=True, choices=STATUS_COURSE)
 
+    # PROFESSIONAL - 4
+    company = models.CharField(max_length=100)
+
     other = MultiSelectField(choices=OTHER, default='', max_length=20)
     situation = models.CharField(max_length=20, default='Pending', choices=SITUATION, null=True,)
     created_on = models.DateTimeField(auto_now_add=True)
