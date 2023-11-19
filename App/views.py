fb0d79fbf4c384d635ca5ee5c2535d2ded349365
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-11-19 14:03:21 +0430

    Add new page

--- App/views.py
+++ App/views.py
@@ -4,19 +4,7 @@ from django.contrib import messages
 from .forms import CandidateForm
 
 def home(request):
-    if request.method == 'POST':
-        form = CandidateForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Your request was successful")
-            return redirect('/')
-        else:
-            messages.error(request, "Something went wrong, please try again")
-        context = {'form': form}
-        return render(request, "home.html", context)
-    else:
-        form = CandidateForm()
-        return render(request, "home.html", {'form': form})
+    return render(request, 'home.html')
 
 def register(request):
     if request.method == 'POST':
@@ -24,7 +12,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Your request was successful")
-            return redirect('/')
+            return redirect('/register')
         else:
             messages.error(request, "Something went wrong, please try again")
         context = {'form': form}
