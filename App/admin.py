ff4916b3da7802b17a18a5b39f08e9200d54430b
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-11-19 10:32:49 +0430

    Update admin fieldsets

--- App/admin.py
+++ App/admin.py
@@ -34,6 +34,11 @@ class CandidateAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_on', 'status', '_')
     search_fields = ('first_name', 'last_name', 'email',)
     
+
+    fieldsets = [
+        ('PERSONAL', {'fields': ['email', 'phone', 'gender',]}),
+    ]
+
     def get_fields(self, request, obj=None):
         fields = super().get_fields(request, obj)
         if obj:
