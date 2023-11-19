60284a8e02f9598eea3a41a616b4bf6c333dd1e1
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-11-19 10:10:49 +0430

    Update remote field in forms

--- App/forms.py
+++ App/forms.py
@@ -162,6 +162,7 @@ class CandidateForm(forms.ModelForm):
         )
     )
     employed = forms.BooleanField(label="I am employed", required=False)
+    remote = forms.BooleanField(label="I agree to work remotely", required=False)
 
     class Meta:
         model = Candidate
