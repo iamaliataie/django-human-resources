53bba66a8b4e24b92383ca46d23f55962612cb4e
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-12-14 16:06:32 +0430

    Update forms.py

--- App/forms.py
+++ App/forms.py
@@ -26,7 +26,7 @@ class CandidateForm(forms.ModelForm):
                 'style': 'text-transform: capitalize',
                 }
             )
-        )
+    )
     
     last_name = forms.CharField(
         label="Last Name",
