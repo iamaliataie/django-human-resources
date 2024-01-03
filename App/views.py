0d7e9907987495d9a861962d711963accbb2fff3
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-01-03 21:12:30 +0430

    Update views.py

--- App/views.py
+++ App/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import CandidateForm
 
+
 def home(request):
     return render(request, 'home.html')
 
