b3e30ea47042fd06b53747c47bf0765c5f1f49e3
Ali Ahmad Ataiee <iamaliataie@gmail.com>
2024-01-28 21:15:05 +0430

    some changes

--- App/admin.py
+++ App/admin.py
@@ -47,8 +47,9 @@ class CandidateAdmin(admin.ModelAdmin):
         )
     exclude = ('status',)
     list_filter = ('situation',)
-    list_display = ('name', 'email', 'created_on', 'status', '_')
     
+    list_display = ('name', 'email', 'created_on', 'status', '_')
+
     search_fields = ('first_name', 'last_name', 'email',)
 
     fieldsets = [
