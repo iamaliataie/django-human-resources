f6ebcc68f145486f65c19f6791c0378ec2f5b8c9
Ali Ahmad Ataiee <iamaliataie@gmail.com>
2024-01-29 21:00:58 +0430

    some changes

--- App/forms.py
+++ App/forms.py
@@ -167,8 +167,9 @@ class CandidateForm(forms.ModelForm):
     )
 
     employed = forms.BooleanField(label="I am employed", required=False)
-    
+
     remote = forms.BooleanField(label="I agree to work remotely", required=False)
+    
     travel = forms.BooleanField(label="I am available to travel", required=False)
     experience = forms.BooleanField(label='I have experience', required=False)
 
