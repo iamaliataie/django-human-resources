14b63e7c9fcb09ca68b8729a422b8bfd13a6b416
Ali Ahmad Ataiee <iamaliataie@gmail.com>
2024-01-29 21:02:04 +0430

    some changes

--- App/forms.py
+++ App/forms.py
@@ -185,7 +185,7 @@ class CandidateForm(forms.ModelForm):
             'started_job': 'Started',
             'finished_job': 'Finished',
         }
-        
+
         SALARY = (
             ('', 'Salary expectation (month)'),
             ('Between  ($3000 and $4000)', 'Between  ($3000 and $4000)'),
@@ -193,6 +193,7 @@ class CandidateForm(forms.ModelForm):
             ('Between  ($5000 and $7000)', 'Between  ($5000 and $7000)'),
             ('Between  ($7000 and $10000)', 'Between  ($7000 and $10000)'),
         )
+        
         GENDER = (
             ('M', 'Male'),
             ('F', 'Female'),
